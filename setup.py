69a61019314b402273343713ff491ab185eeeefa
Vaclav Klecanda <vencax77@gmail.com>
2012-03-11 10:23:29 +0100

    fixes

--- setup.py
+++ setup.py
@@ -4,24 +4,24 @@ import os
 
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README')
 
-description = 'Admininstration module for SQL based Linux NSS authentication system'
+description = 'Login script generator for windoze samba users stored in SQL DB.'
 
 if os.path.exists(README_PATH):
     long_description = open(README_PATH).read()
 else:
     long_description = description
 
-setup(name='django-sql-nss-admin',
+setup(name='django-samba-admin',
     version='0.1',
     description=description,
     license='BSD',
-    url='https://github.com/vencax/django-vxk-forum',
+    url='https://github.com/vencax/django-samba-admin',
     author='vencax',
     author_email='vencax@centrum.cz',
     packages=find_packages(),
     install_requires=[
         'django>=1.3',
     ],
-    keywords="django linux libnss-mysql nss admin",
+    keywords="django linux samba admin",
     include_package_data=True,
 )
